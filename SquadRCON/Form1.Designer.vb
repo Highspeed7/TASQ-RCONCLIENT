c17661fef52e547f0cb639bdfef1d363ce5e3f34
Felix <dsmrunnah@yahoo.com>
2022-08-16 17:42:04 -0400

    Remote Connection (RCON) for Dedicated Game Servers

---
+++ SquadRCON/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.tbCommand = New System.Windows.Forms.TextBox()
+        Me.lbRdyForCommand = New System.Windows.Forms.Label()
+        Me.lbOutput = New System.Windows.Forms.Label()
+        Me.rtbOutput = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(63, 56)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(215, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(63, 107)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(215, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(60, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(17, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "IP"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(60, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(26, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Port"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(63, 160)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(215, 20)
+        Me.TextBox3.TabIndex = 4
+        Me.TextBox3.UseSystemPasswordChar = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(60, 144)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Password"
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(63, 199)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(94, 31)
+        Me.btnConnect.TabIndex = 6
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'tbCommand
+        '
+        Me.tbCommand.Location = New System.Drawing.Point(63, 294)
+        Me.tbCommand.Name = "tbCommand"
+        Me.tbCommand.Size = New System.Drawing.Size(941, 20)
+        Me.tbCommand.TabIndex = 7
+        '
+        'lbRdyForCommand
+        '
+        Me.lbRdyForCommand.AutoSize = True
+        Me.lbRdyForCommand.Location = New System.Drawing.Point(60, 278)
+        Me.lbRdyForCommand.Name = "lbRdyForCommand"
+        Me.lbRdyForCommand.Size = New System.Drawing.Size(109, 13)
+        Me.lbRdyForCommand.TabIndex = 8
+        Me.lbRdyForCommand.Text = "Ready For Command:"
+        '
+        'lbOutput
+        '
+        Me.lbOutput.AutoSize = True
+        Me.lbOutput.Location = New System.Drawing.Point(60, 336)
+        Me.lbOutput.Name = "lbOutput"
+        Me.lbOutput.Size = New System.Drawing.Size(42, 13)
+        Me.lbOutput.TabIndex = 14
+        Me.lbOutput.Text = "Output:"
+        '
+        'rtbOutput
+        '
+        Me.rtbOutput.BackColor = System.Drawing.SystemColors.Control
+        Me.rtbOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.rtbOutput.Location = New System.Drawing.Point(63, 352)
+        Me.rtbOutput.Name = "rtbOutput"
+        Me.rtbOutput.Size = New System.Drawing.Size(941, 254)
+        Me.rtbOutput.TabIndex = 15
+        Me.rtbOutput.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(1054, 723)
+        Me.Controls.Add(Me.rtbOutput)
+        Me.Controls.Add(Me.lbOutput)
+        Me.Controls.Add(Me.lbRdyForCommand)
+        Me.Controls.Add(Me.tbCommand)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Squad RCON"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents tbCommand As TextBox
+    Friend WithEvents lbRdyForCommand As Label
+    Friend WithEvents lbOutput As Label
+    Friend WithEvents rtbOutput As RichTextBox
+End Class
---
+++ SquadRCON/Form1.vb
@@ -0,0 +1,124 @@
+﻿Imports System
+Imports System.Net
+Imports System.Net.IPAddress
+Imports System.Threading
+Imports SquadRCON
+Imports SquadRCON.SourceRcon
+
+
+Public Class Form1
+
+
+    Dim ipaddress, password, command As String
+    Dim port As Integer
+    Dim MesssageString As String
+    Public Srcon As New SourceRcon()
+
+    Public Sub Invoke(ByVal control As Control, ByVal action As Action)
+        If control.InvokeRequired Then
+            control.Invoke(New MethodInvoker(Sub() action()), Nothing)
+        Else
+            action.Invoke()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lbRdyForCommand.Enabled = False
+        tbCommand.Enabled = False
+        lbOutput.Enabled = False
+        rtbOutput.Enabled = False
+
+
+    End Sub
+
+
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+
+
+
+        ipaddress = TextBox1.Text
+        port = TextBox2.Text
+        password = TextBox3.Text
+
+
+        Dim IP As Long = GetIPAddress(ipaddress)
+
+
+        AddHandler Srcon.Errors, AddressOf ErrorOutput
+        AddHandler Srcon.ServerOutput, AddressOf ReadOutput
+
+        If Srcon.Connected = False Then
+            If Srcon.Connect(New IPEndPoint(IP, port), password) Then
+
+                While Srcon.Connected = True
+                    Thread.Sleep(5)
+                End While
+
+                lbRdyForCommand.Enabled = True
+                tbCommand.Enabled = True
+                lbOutput.Enabled = True
+                rtbOutput.Enabled = True
+            End If
+        End If
+
+
+
+    End Sub
+
+
+
+    Private Sub tbCommand_KeyDown(sender As Object, e As KeyEventArgs) Handles tbCommand.KeyDown
+        command = tbCommand.Text
+
+        If (e.KeyData = Keys.Enter) Then
+            Srcon.ServerCommand(command)
+            Thread.Sleep(1000)
+            Return
+        End If
+
+
+
+    End Sub
+
+
+
+    Private Sub ErrorOutput(input As String)
+        Dim sInput As String
+        sInput = input
+
+
+        'For Each sInput In input
+        'tbOutput.Invoke(Sub() tbOutput.Text = sInput)
+        'Next
+        'MessageBox.Show(input, CStr(vbOK))
+        rtbOutput.Invoke(Sub() rtbOutput.Text = sInput)
+
+    End Sub
+
+
+
+    Private Sub ReadOutput(input As String)
+        Dim sInput As String
+        sInput = input
+
+        'MessageBox.Show(input, CStr(vbOK))
+        rtbOutput.Invoke(Sub() rtbOutput.Text = sInput)
+
+    End Sub
+
+
+    Public Function GetIPAddress(ByRef URL As String) As Long
+        Dim UrlEntry As IPHostEntry = Dns.GetHostEntry(URL)
+        Dim IP As IPAddress() = Dns.GetHostAddresses(URL)
+        Dim IPLong As Long
+
+        IPLong = IP.GetValue(0).Address
+        Return IPLong
+
+    End Function
+
+
+
+End Class
+
+
